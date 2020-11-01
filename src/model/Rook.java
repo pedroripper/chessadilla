c7f87808c73c6f16e1a71f7b2119366ef630c80d
Gabriel Manhães <gabrielmanhaes@tecgraf.puc-rio.br>
2020-11-01 17:02:10 -0300

    Adicionando move_list da torre

--- src/model/Rook.java
+++ src/model/Rook.java
@@ -8,10 +8,29 @@ class Rook extends Piece{
 		super(c, x, y);
 	}
 
-	@Override
 	public ArrayList<Coordinate> move_list() throws CoordinateInvalid {
-		// TODO Auto-generated method stub
-		return null;
+		int i = 0;
+		ArrayList<Coordinate> lst = new ArrayList<Coordinate>();
+		int[][] orientation = {
+				{1,0},
+				{-1,0},
+				{0,1},
+				{0,-1},
+		};
+		while (orientation[i] != null) {
+			int j = 1;
+			while (board.get_piece(this.coord.x + j*orientation[i][0], 
+								   this.coord.y + j*orientation[i][1]) == null &&
+				   board.verify_xy(this.coord.x + j*orientation[i][0],
+								   this.coord.y + j*orientation[i][1])) {
+				lst.add(new Coordinate(this.coord.x + j*orientation[i][0],
+									   this.coord.y + j*orientation[i][1]));
+				j++;
+			}
+			i++;
+		}
+		this.moveList = lst;
+		return lst;
 	}
 
 
