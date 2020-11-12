3f29683a1816ace39bf92db6daf08d48e69af4d8
Pedro Ripper <pedros.ripper@globo.com>
2020-11-12 20:54:31 -0300

    trying image

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,9 +1,11 @@
 package Main;
+import java.net.URISyntaxException;
+
 import view.StartingFrame;
 
 public class Main {
 	
-	public static void main(String []args) {
+	public static void main(String []args) throws URISyntaxException {
 //		System.out.printl;
 		StartingFrame f = new StartingFrame();
 		f.setVisible(true);
--- src/view/StartingFrame.java
+++ src/view/StartingFrame.java
@@ -1,21 +1,32 @@
 package view;
 
-import java.awt.Color;
-import java.awt.Font;
+import java.awt.*;
+import java.io.*;
+import java.net.URISyntaxException;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class StartingFrame extends JFrame  {
 	JButton b_newGame = new JButton("Novo Jogo");
 	JButton b_loadGame = new JButton("Carregar Jogo");
+	Image logo;
 	JLabel title = new JLabel("Xadrez");
 	JPanel p = new JPanel();
 	
-	public StartingFrame(){
+	public StartingFrame() throws URISyntaxException{
+		try {
+			logo = ImageIO.read(new File(getClass().getResource("/pices-1/b_bispo.gif").toURI()));
+			}
+		catch(IOException e) {
+			System.out.println("oa");
+			System.out.println(e.getMessage());
+			System.exit(1); 
+		}
 		title.setFont(new Font("Verdana", Font.PLAIN, 90));
 //		title.setVerticalAlignment(SwingConstants.CENTER);
 		p.add(title);
-		p.add(new JLabel(new ImageIcon("img/main.png")));
+//		p.add(new JLabel(new ImageIcon("./img/main.png")));
 		p.add(b_newGame);
 		p.add(b_loadGame);
 		p.setBackground(Color.WHITE);
